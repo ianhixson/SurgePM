12932eb1ae88f6eec590a9978d8618c120f6d0b6
Nick Budak <thatbudakguy@gmail.com>
2016-03-22 19:46:32 -0700

    initial commit
    adds html, js, zone names

---
+++ api_query.js
@@ -0,0 +1,17 @@
+/*
+var xhr = new XMLHttpRequest();
+xhr.open('GET', 'https://api.uber.com/v1/products?latitude=37.7759792&longitude=-122.41823');
+xhr.setRequestHeader("Authorization", "qJxLLD_mhPA3vQYt-z3nYoNxRuZpuP2Xcl8jXiat");
+xhr.send();
+*/
+
+var response = $.ajax({
+  type:"GET",
+  url:"https://api.uber.com/v1/products?latitude=37.7759792&longitude=-122.41823",
+  crossDomain: true,
+  beforeSend: function (request)
+  {
+    request.setRequestHeader("Authorization", "qJxLLD_mhPA3vQYt-z3nYoNxRuZpuP2Xcl8jXiat");
+  }
+});
+console.log(response);
