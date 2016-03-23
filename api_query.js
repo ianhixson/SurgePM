6247b5026f95eedde398ceaa2af473316727b4de
thatbudakguy <thatbudakguy@gmail.com>
2016-03-23 00:04:07 -0700

    add styling and input

--- api_query.js
+++ api_query.js
@@ -5,17 +5,27 @@ xhr.setRequestHeader("Authorization", "qJxLLD_mhPA3vQYt-z3nYoNxRuZpuP2Xcl8jXiat"
 xhr.send();
 */
 
-var response = $.ajax({
-  type:"GET",
-  url:"https://api.uber.com/v1/products?latitude=37.7759792&longitude=-122.41823",
-  crossDomain: true,
-  beforeSend: function (request)
-  {
-    request.setRequestHeader("Authorization", "Token qJxLLD_mhPA3vQYt-z3nYoNxRuZpuP2Xcl8jXiat");
-  },
-  complete: function (request)
-  {
-    $('#results').html(JSON.stringify(response));
-    console.debug(response);
-  }
+$('#go').click(function(e){
+
+  e.preventDefault();
+
+  var startlat = $('#startlat').text();
+  var startlong = $('#startlong').text();
+  var formedurl = "https://api.uber.com/v1/products?latitude=" + startlat + "&longitude=" + startlong;
+
+  $.ajax({
+    type:"GET",
+    url: formedurl,
+    crossDomain: true,
+    beforeSend: function (request)
+    {
+      request.setRequestHeader("Authorization", "Token qJxLLD_mhPA3vQYt-z3nYoNxRuZpuP2Xcl8jXiat");
+    },
+    complete: function (request)
+    {
+      $('#results').html(JSON.stringify(response));
+      console.debug(response);
+    }
+  });
+
 });
